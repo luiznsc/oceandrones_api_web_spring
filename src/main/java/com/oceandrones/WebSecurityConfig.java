04c7a6fa2c33a1c88793aab76aca2f289e2aacc4
luiznsc <luizhenriquensc@gmail.com>
2024-06-06 19:07:27 -0300

    update

--- src/main/java/com/oceandrones/WebSecurityConfig.java
+++ src/main/java/com/oceandrones/WebSecurityConfig.java
@@ -10,15 +10,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-	@Bean
-	public SecurityFilterChain securtityFilterChain(HttpSecurity http) throws Exception{
-		http.authorizeHttpRequests()
-				.requestMatchers("/usuarios/cadastrar", "/usuarios/buscar").permitAll()
-				.anyRequest().authenticated()
-				.and().httpBasic();
-		http.csrf().disable();
-		return http.build();
-	}
+    @Bean
+    public SecurityFilterChain securtityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests()
+                .requestMatchers("/usuarios/cadastrar", "/usuarios/buscar").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().permitAll()
+                .and().httpBasic();
+        http.csrf().disable();
+        return http.build();
+    }
 
 
 	/*@Bean
@@ -32,9 +33,3 @@ public class WebSecurityConfig {
 		return new InMemoryUserDetailsManager(user);
 	}*/
 }
-
-
-
-
-
-
