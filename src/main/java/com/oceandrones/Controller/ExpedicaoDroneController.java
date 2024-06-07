62507abef192fd441d974e6847ebd962fcd45548
luiznsc <luizhenriquensc@gmail.com>
2024-06-07 11:38:02 -0300

    ExpedicaoControler

--- src/main/java/com/oceandrones/Controller/ExpedicaoDroneController.java
+++ src/main/java/com/oceandrones/Controller/ExpedicaoDroneController.java
@@ -3,13 +3,8 @@ package com.oceandrones.Controller;
 import com.oceandrones.ExpedicaoDrone.ExpedicaoDrone;
 import com.oceandrones.ExpedicaoDrone.IExpedicaoDroneRepository;
 import com.oceandrones.ExpedicaoDrone.RDadosCadastroExpedicaoDrone;
-import com.oceandrones.ExpedicaoDrone.RDadosListagemExpedicaoDrone;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -28,17 +23,6 @@ public class ExpedicaoDroneController {
     public void cadastroExpedicaoDrone(@RequestBody @Valid RDadosCadastroExpedicaoDrone dadosCadastroExpedicaoDrone){
         expedicaoDroneRepository.save(new ExpedicaoDrone(dadosCadastroExpedicaoDrone));
     }
-
-
-    @GetMapping("/buscar/{idExpDrone}")
-    public ResponseEntity<Page<RDadosListagemExpedicaoDrone>> lista(
-            @PathVariable Long idExpDrone,
-            @PageableDefault(size = 10) Pageable paginacao){
-        Page<ExpedicaoDrone> expedicaoDrones = expedicaoDroneRepository.findAllByIdExpDrone(idExpDrone, paginacao);
-        Page<RDadosListagemExpedicaoDrone> expedicaoDados = expedicaoDrones.map(RDadosListagemExpedicaoDrone::new);
-        return ResponseEntity.ok(expedicaoDados);
-    }
-
 
     @DeleteMapping("/deletar/{idExpDrone}")
     @Transactional
